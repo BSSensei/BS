8161cd536602b1459fc8ca04adea86f547ab54f5
BSSensei <Senseiba1314@163.com>
2026-05-17 13:15:40 +0800

    Update Toast.swift

--- Toast.swift
+++ Toast.swift
@@ -1,24 +1,40 @@
-// Toast.swift
 import UIKit
 
 class Toast {
-    static func show(_ message: String, on view: UIView) {
+    static func show(_ message: String, on view: UIView, duration: TimeInterval = 2.0) {
+        let container = UIView()
+        container.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        container.layer.cornerRadius = 10
+        container.clipsToBounds = true
+        container.translatesAutoresizingMaskIntoConstraints = false
+        
         let label = UILabel()
         label.text = message
         label.textColor = .white
-        label.backgroundColor = UIColor.black.withAlphaComponent(0.8)
-        label.textAlignment = .center
         label.font = .systemFont(ofSize: 14)
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        label.frame = CGRect(x: 0, y: 0, width: 200, height: 36)
-        label.center = view.center
-        view.addSubview(label)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
         
-        UIView.animate(withDuration: 0.5, delay: 2.0, options: .curveEaseOut) {
-            label.alpha = 0
+        container.addSubview(label)
+        view.addSubview(container)
+        
+        NSLayoutConstraint.activate([
+            container.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            container.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            container.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 40),
+            container.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -40),
+            
+            label.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+            label.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
+            label.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -8)
+        ])
+        
+        UIView.animate(withDuration: 0.3, delay: duration, options: .curveEaseOut) {
+            container.alpha = 0
         } completion: { _ in
-            label.removeFromSuperview()
+            container.removeFromSuperview()
         }
     }
 }
